60de8e86ab6f281d20b8668e9176eafe1ff07ae7
shlyambur <shlyambur@gmail.com>
2016-05-15 19:11:18 +0300

    Update solo.rb

--- solo.rb
+++ solo.rb
@@ -4,4 +4,5 @@
 root = File.absolute_path(File.dirname(__FILE__))
  
 file_cache_path root + "/cache/"
-cookbook_path root + '/cookbooks/new'
+#cookbook_path root + '/cookbooks/new'
+cookbook_path "~/test/chef/cookbooks/new"
